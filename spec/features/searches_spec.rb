2a78c9a6d42a73806ddb173223743ffd3dcdcd58
Mark Dillon <mdillon@gmail.com>
2015-08-22 22:48:59 -0400

    back to example keyword

--- spec/features/searches_spec.rb
+++ spec/features/searches_spec.rb
@@ -10,7 +10,7 @@ feature "Looking up recipes", js: true do
 
   scenario "finding recipes" do
     visit '/'
-    fill_in "keywords", with: "aked"
+    fill_in "keywords", with: "baked"
     click_on "Search"
 
     expect(page).to have_content("Baked Potato")
