47ba8b3f1b815b485e4795c8ba4c0eb1843d758f
Mark Dillon <mdillon@gmail.com>
2015-08-23 06:21:15 -0400

    show view and passing tests

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -8,4 +8,8 @@ class RecipesController < ApplicationController
       []
     end
   end
+
+  def show
+    @recipe = Recipe.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  resources :recipes, only: [:index]
   root 'home#index'
+  resources :recipes, only: [:index, :show]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
